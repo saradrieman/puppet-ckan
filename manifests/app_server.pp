fb72a58fd586354763d6a2b364d8aa7c2cabf8ab
Simon Sellar <simon.sellar@ersa.edu.au>
2015-01-29 09:23:54 +1030

    Created template for classes.

---
+++ manifests/app_server.pp
@@ -0,0 +1,6 @@
+# Configures the CKAN App Server
+class ckan::app_server {
+}
+
+# vim: set shiftwidth=2 softtabstop=2 textwidth=0 wrapmargin=0:
+
---
+++ manifests/init.pp
@@ -0,0 +1,6 @@
+# Default class for ckan installation - installs everything on a single host.
+class ckan {
+}
+
+# vim: set shiftwidth=2 softtabstop=2 textwidth=0 wrapmargin=0:
+
