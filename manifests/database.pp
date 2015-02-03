72af194c51126e908ae3fad663a35dedd3d79d88
Simon Sellar <simon.sellar@ersa.edu.au>
2015-02-03 12:28:27 +1030

    Worked through issues.

--- manifests/database.pp
+++ manifests/database.pp
@@ -19,15 +19,15 @@ class ckan::database (
   $postgres_password     = undef,
 
   # IP Address of the CKAN App Server
-  $ckan_app_ip           = undef
+  $ckan_app_ip           = undef,
 
   # Database encoding
-  $db_encoding           = 'utf-8',
+  $db_encoding           = 'utf-8'
 
 ) {
   # Postgresql installation.
   class { '::postgresql::server':
-    postgres_password => "${postgres_password}"
+    postgres_password => "${postgres_password}",
     ipv4acls          => [
       "host ${ckan_db_name} ${ckan_db_user} ${ckan_app_ip}/32 md5",
       "host ${datastore_db_name} ${ckan_db_user} ${ckan_app_ip}/32 md5",
